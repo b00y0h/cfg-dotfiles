6a13426e6cc83f3d0dea999b79912672c2cdee17
Bobby Smith <reversetype@gmail.com>
2020-03-06 15:49:39 -0500

    removed iterm related items

--- .brew.sh
+++ .brew.sh
@@ -11,6 +11,10 @@ else
     brew update
 fi
 
+which -s brew
+if [[ $? != 0 ]] ; then
+    printf "Homebrew not installed"
+else
 # Upgrade any already-installed formulae
 brew upgrade
 
@@ -18,7 +22,7 @@ brew install shpotify
 brew install tmux
 brew install youtube-dl
 brew install libvpx
-brew install ffmpeg --with-fdk-aac --with-tools --with-freetype --with-libass --with-libvorbis --with-libvpx --with-x265
+brew install ffmpeg --with-tools --with-freetype --with-libass --with-libvorbis --with-libvpx --with-x265
 brew install rmtrash
 brew install tree
 
@@ -27,18 +31,8 @@ brew install z
 
 # brew install vim --with-override-system-vi
 
-brew install git
+# brew install git
 
 # Remove outdated versions from the cellar
 brew cleanup
-
-# Install mac apps
-brew cask install iterm2
-
-# set up iterm (duplicated in .osx file but need it here b/c sometimes I don't run .osx
-# Don't display the annoying prompt when quitting iTerm
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false
-
-# load profile from dotfiles
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm-profile"
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
